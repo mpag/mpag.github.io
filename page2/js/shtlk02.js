da689c4b6ab6d526f8cecb72dd2612784614b0cf
mpag <mitchell.r.page@gmail.com>
2018-08-15 00:11:22 +1000

    Updating JSON and Axo Looping Function
    Updated JSON Geometry reflecting latest building. Addition of more succinct part position y function that loops through imported json file geometry objects and exponential assigns vertical offset. Gradient change to background

--- page2/js/shtlk02.js
+++ page2/js/shtlk02.js
@@ -7,6 +7,7 @@ var screenHeight = window.innerHeight;
 var aspect = screenWidth / screenHeight;
 var frustumSize = 1000;
 var slider = document.getElementById("myRange");
+var objectMove = [];    
 
 
 window.addEventListener( 'resize', onWindowResize, false );
@@ -25,11 +26,12 @@ function init(){
   camera.zoom = 4;
   camera.updateProjectionMatrix();
 
-  var planeGeometry = new THREE.PlaneGeometry( 500, 500, 32 );
+  var planeGeometry = new THREE.PlaneGeometry( 500, 500, 64 );
   planeGeometry.rotateX( - Math.PI / 2 );
   planeGeometry.rotateY( - Math.PI / 4 );
   var planeMaterial = new THREE.ShadowMaterial();
   var plane = new THREE.Mesh( planeGeometry, planeMaterial );
+  plane.position.y = -65;
   plane.receiveShadow = true;
   scene.add( plane );
 
@@ -37,7 +39,7 @@ function init(){
   //////// GEOMETRY /////////
   var index = 0;
   var countLoaded = 0;
-  var files = ["models/inquiry0.json", "models/inquiry1.json", "models/inquiry2.json"];
+  var files = ["models/baseTopo.json", "models/baseStructure01.json", "models/baseStructure02.json", "models/baseStructure03.json", "models/baseStructure04.json", "models/baseStructure05.json", "models/baseFacade01.json", "models/baseFacade02.json", "models/baseFacade03.json", "models/baseFacade04.json", "models/baseFacade05.json"];
 
   //////////LOADER///////////
 
@@ -75,7 +77,9 @@ function init(){
         });
         object.name = "part" + index;
         object.rotation.x = -Math.PI / 2;
+        object.position.y = -65;
         scene.add(object);
+        objectMove.push(scene.getObjectByName("part" + index));
         index++;
         loadNextFile();
     }, onProgress, onError);
@@ -83,21 +87,36 @@ function init(){
 
   loadNextFile();
 
+  // for (var i = 0; i < (files.length-1); i++) {
+  //   var objectMove = scene.getObjectByName("part" + i);
+  //   console.log(objectMove);
+  // }
+  // var part0 = scene.getObjectByName("part0");
+  // var part1 = scene.getObjectByName("part1");
+  // var part2 = scene.getObjectByName("part2");
+  // var part3 = scene.getObjectByName("part3");
+  // var part4 = scene.getObjectByName("part4");
+  // var part5 = scene.getObjectByName("part5");
+  // var part6 = scene.getObjectByName("part6");
+  // var part7 = scene.getObjectByName("part7");
+  // var part8 = scene.getObjectByName("part8");
+  // var part9 = scene.getObjectByName("part9");
+
 
   //LIGHT
-  var ambientlight = new THREE.AmbientLight( 0x080808, 5 ); 
-  var directionalLight = new THREE.DirectionalLight( 0xffffff, 1.5 );
-  directionalLight.shadow.camera.right =  500;
-  directionalLight.shadow.camera.left = -500;
-  directionalLight.shadow.camera.top =  500;
-  directionalLight.shadow.camera.bottom = -500;
-  directionalLight.position.y = 150;
-  directionalLight.position.z = -50;
-  directionalLight.position.x = -50;
+  var ambientlight = new THREE.AmbientLight( 0x080808, 20 ); 
+  var directionalLight = new THREE.DirectionalLight( 0xffffff, 1.0 );
+  directionalLight.shadow.camera.right =  1000;
+  directionalLight.shadow.camera.left = -1000;
+  directionalLight.shadow.camera.top =  1000;
+  directionalLight.shadow.camera.bottom = -1000;
+  directionalLight.position.y = 300;
+  directionalLight.position.z = -100;
+  directionalLight.position.x = -100;
   directionalLight.shadow.mapSize.width = 1024;  // default
   directionalLight.shadow.mapSize.height = 1024; // default
-  directionalLight.shadow.camera.near = -100;    // default
-  directionalLight.shadow.camera.far = 1500;     // default
+  directionalLight.shadow.camera.near = 0;    // default
+  directionalLight.shadow.camera.far = 2500;     // default
   directionalLight.castShadow = true;
   scene.add( directionalLight );
   scene.add( ambientlight );
@@ -115,7 +134,7 @@ function init(){
     if (isMobileDevice() == true){
       renderer.setPixelRatio( 1 );
     } else {
-      renderer.setPixelRatio( 1.5 );
+      renderer.setPixelRatio( 2.0 );
     }
   };
   setPixelRatio();
@@ -124,33 +143,28 @@ function init(){
   controls = new THREE.OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
 	controls.dampingFactor = 0.8;
+  controls.autoRotate = true;
   controls.update();
+
+  
 
   //ANIMATION/EVERY FRAME LOOP
   function animation(){
     window.requestAnimationFrame( animation );
     // stats.begin();
-    
-    //VARIABLES
-    var part0 = scene.getObjectByName("part0");
-    var part1 = scene.getObjectByName("part1");
-    var part2 = scene.getObjectByName("part2");
-
     slider.oninput = function() {
-      var sliderNum = this.value / 5;
-      plane.position.y = sliderNum* -5
-      part0.position.y = sliderNum* -5;
-      part1.position.y = sliderNum* 1;
-      part2.position.y = sliderNum* 6;
-      camera.zoom = 4 - sliderNum/ 12;
+      var sliderNum = this.value / 1.5;
+      for (var i = 6; i < (objectMove.length - 1); i++) {
+        objectMove[i].position.y = -65 + (sliderNum / 10) * Math.pow(i, 1.5);
+      };
+      camera.zoom = 4 - sliderNum / 60;
     };
- 
     camera.updateProjectionMatrix();
     controls.update();
-    
     renderer.render( scene, camera);
     // stats.end();
-  };  
+  };
+
 
   animation();  
 };
