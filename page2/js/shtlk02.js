b658b61367108fe14b8fde95bb48c6e4c2b4a1b7
mpag <mitchell.r.page@gmail.com>
2018-08-26 13:47:45 +1000

    Addition of CSS3D Renderer and Annotation Text
    Addition of CSS3D render with axonometric text. Slider control of vertical displacement of text achieved.

--- page2/js/shtlk02.js
+++ page2/js/shtlk02.js
@@ -1,22 +1,41 @@
 
 
-var camera, scene, renderer, meshKnot, meshPlane, meshBox;
+//Global Variables
+var camera, scene, renderer, rectangle, div, controls, element;
+var scene2, renderer2, manager;
+var line = []; 
+var divs = [];
+var divText = [];
+window.addEventListener( 'resize', onWindowResize, false );
+
+//Loader Variables
+var index = 0;
+var countLoaded = 0;
+var files = ["models/baseTopo.json", "models/baseStructure01.json", "models/baseStructure02.json", "models/baseStructure03.json", "models/baseStructure04.json", "models/baseStructure05.json", "models/baseFacade01.json", "models/baseFacade02.json", "models/baseFacade03.json", "models/baseFacade04.json", "models/baseFacade05.json"];
+
+
+//Init Variables
 var count = 0;
 var screenWidth = window.innerWidth; 
 var screenHeight = window.innerHeight;
 var aspect = screenWidth / screenHeight;
 var frustumSize = 1000;
 var slider = document.getElementById("myRange");
-var objectMove = [];    
+var objectMove = [];
 
-
-window.addEventListener( 'resize', onWindowResize, false );
 
 init();
+animate(); 
+uiReshuffle();
+
 
 function init(){
-  //CAMERA
+  //Scenes
   scene = new THREE.Scene();
+  scene2 = new THREE.Scene();
+  
+
+  //CAMERA
   near = -100; 
   far = 10000;
   camera = new THREE.OrthographicCamera( frustumSize*aspect/-2, frustumSize*aspect/2, frustumSize/2, frustumSize/-2, near, far );
@@ -26,36 +45,76 @@ function init(){
   camera.zoom = 4;
   camera.updateProjectionMatrix();
 
+
+  //////// GEOMETRY /////////
+
   var planeGeometry = new THREE.PlaneGeometry( 500, 500, 64 );
   planeGeometry.rotateX( - Math.PI / 2 );
   planeGeometry.rotateY( - Math.PI / 4 );
   var planeMaterial = new THREE.ShadowMaterial();
   var plane = new THREE.Mesh( planeGeometry, planeMaterial );
-  plane.position.y = -65;
+  plane.opacity = 0.5;
+  plane.position.y = -70;
   plane.receiveShadow = true;
   scene.add( plane );
 
 
-  //////// GEOMETRY /////////
-  var index = 0;
-  var countLoaded = 0;
-  var files = ["models/baseTopo.json", "models/baseStructure01.json", "models/baseStructure02.json", "models/baseStructure03.json", "models/baseStructure04.json", "models/baseStructure05.json", "models/baseFacade01.json", "models/baseFacade02.json", "models/baseFacade03.json", "models/baseFacade04.json", "models/baseFacade05.json"];
+  /////////CSS GEOM//////////
+  element = document.createElement('div');
+  element.className = "tag";
+  elText = document.createElement('div');
+  element.appendChild( elText );
+  elText.className = "titleText";
+  elText.innerHTML = 'STEM SCHOOL';
+
+  titleDiv = new THREE.CSS3DObject(element);
+  titleDiv.position.x = -105;
+  titleDiv.position.y = -70;
+  titleDiv.position.z = -66;
+  titleDiv.rotation.x = -Math.PI / 2;
+  titleDiv.rotation.z =  Math.PI;
+  scene2.add(titleDiv);
+
+
+  for (i = 0; i < 4; i++){
+    elementFacade = document.createElement('div');
+    elementFacade.className = "tag";
+    elTextFacade = document.createElement('div');
+    elementFacade.appendChild( elTextFacade );
+    elTextFacade.className = "subText";
+    elTextFacade.id = "model" + i ;
+    elTextFacade.innerHTML = "COLOUR TYPE " + i;
+    divText.push( elementFacade );
+  };
+
+  for (i = 0; i < divText.length; i++){
+    subText = new THREE.CSS3DObject( divText[i] );
+    divs.push(subText);
+    subText.name = "cssModel" + i;
+    subText.position.x = -105;
+    subText.position.y = -70;
+    subText.position.z = -66;
+    subText.rotation.x = -Math.PI / 2;
+    subText.rotation.z =  Math.PI;
+    console.log(subText);
+    scene2.add(subText);
+  };
+
 
   //////////LOADER///////////
 
-  var manager = new THREE.LoadingManager();
+  manager = new THREE.LoadingManager();
+  var jsonLoader = new THREE.ObjectLoader( manager );
+
 
   var onProgress = function ( xhr ) {
     if ( xhr.lengthComputable ) {
       var percentComplete = xhr.loaded / xhr.total * 100;
     }};
-
   var onError = function ( xhr ) {
   };
-  
   manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
   };
-  
   manager.onProgress = function ( item, loaded, total ) {
     countLoaded++;
   };
@@ -64,7 +123,6 @@ function init(){
     };
   };
 
-  var jsonLoader = new THREE.ObjectLoader( manager );
   
   function loadNextFile() {
     if (index > files.length - 1) return;
@@ -87,21 +145,6 @@ function init(){
 
   loadNextFile();
 
-  // for (var i = 0; i < (files.length-1); i++) {
-  //   var objectMove = scene.getObjectByName("part" + i);
-  //   console.log(objectMove);
-  // }
-  // var part0 = scene.getObjectByName("part0");
-  // var part1 = scene.getObjectByName("part1");
-  // var part2 = scene.getObjectByName("part2");
-  // var part3 = scene.getObjectByName("part3");
-  // var part4 = scene.getObjectByName("part4");
-  // var part5 = scene.getObjectByName("part5");
-  // var part6 = scene.getObjectByName("part6");
-  // var part7 = scene.getObjectByName("part7");
-  // var part8 = scene.getObjectByName("part8");
-  // var part9 = scene.getObjectByName("part9");
-
 
   //LIGHT
   var ambientlight = new THREE.AmbientLight( 0x080808, 20 ); 
@@ -110,13 +153,13 @@ function init(){
   directionalLight.shadow.camera.left = -1000;
   directionalLight.shadow.camera.top =  1000;
   directionalLight.shadow.camera.bottom = -1000;
-  directionalLight.position.y = 300;
+  directionalLight.position.y = 200;
   directionalLight.position.z = -100;
   directionalLight.position.x = -100;
-  directionalLight.shadow.mapSize.width = 1024;  // default
-  directionalLight.shadow.mapSize.height = 1024; // default
-  directionalLight.shadow.camera.near = 0;    // default
-  directionalLight.shadow.camera.far = 2500;     // default
+  directionalLight.shadow.mapSize.width = 2048;
+  directionalLight.shadow.mapSize.height = 2048;
+  directionalLight.shadow.camera.near = 0;
+  directionalLight.shadow.camera.far = 2500;
   directionalLight.castShadow = true;
   scene.add( directionalLight );
   scene.add( ambientlight );
@@ -129,48 +172,73 @@ function init(){
   renderer.setSize( screenWidth, screenHeight);
   renderer.autoClear = false;
   renderer.setClearColor( 0xffffff, 0);
+  renderer.domElement.style.zIndex = 1;
+  // renderer.domElement.style.pointerEvents= 'none';
+  document.body.appendChild( renderer.domElement );
 
-  function setPixelRatio(){
-    if (isMobileDevice() == true){
-      renderer.setPixelRatio( 1 );
-    } else {
-      renderer.setPixelRatio( 2.0 );
-    }
-  };
-  setPixelRatio();
+  renderer2 = new THREE.CSS3DRenderer();
+  renderer2.setSize(window.innerWidth, window.innerHeight);
+  renderer2.domElement.style.position = 'absolute';
+  renderer2.domElement.style.top = 0;
+  renderer2.domElement.style.zIndex = -1;
+  document.body.appendChild(renderer2.domElement);
 
-  document.getElementById("example1").appendChild( renderer.domElement );
+  //CONTROLS
   controls = new THREE.OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
-	controls.dampingFactor = 0.8;
-  controls.autoRotate = true;
+  controls.dampingFactor = 0.8;
   controls.update();
-
   
-
-  //ANIMATION/EVERY FRAME LOOP
-  function animation(){
-    window.requestAnimationFrame( animation );
-    // stats.begin();
-    slider.oninput = function() {
-      var sliderNum = this.value / 1.5;
-      for (var i = 6; i < (objectMove.length - 1); i++) {
-        objectMove[i].position.y = -65 + (sliderNum / 10) * Math.pow(i, 1.5);
-      };
-      camera.zoom = 4 - sliderNum / 60;
-    };
-    camera.updateProjectionMatrix();
-    controls.update();
-    renderer.render( scene, camera);
-    // stats.end();
-  };
-
-
-  animation();  
 };
 
 
 //////FUNCTIONS////////
+
+function animate(){
+  window.requestAnimationFrame( animate );
+  // stats.begin();
+  slider.oninput = function() {
+    var sliderNum = this.value / 1.5;
+
+    for (var i = 6; i < (objectMove.length - 1); i++) {
+      objectMove[i].position.y = -65 + (sliderNum / 10) * Math.pow(i, 1.5);
+    };
+
+    for (i = 0; i < divs.length; i++){
+      divs[i].position.y = objectMove[i+6].position.y + (sliderNum / 10) * Math.pow(i, 1.5);
+    }
+
+    camera.zoom = 4 - sliderNum / 60;
+
+    //Create Line Element between Dynamically Moving Objects
+    scene.remove(line);
+    var geometryLine = new THREE.Geometry();
+    geometryLine.vertices.push(new THREE.Vector3( div.position.x - 17, div.position.y - 15, div.position.z) );
+    geometryLine.vertices.push(new THREE.Vector3( meshKnot.position.x + 20, meshKnot.position.y, meshKnot.position.z) );
+    var lineMaterial = new THREE.LineBasicMaterial( { color: 0x9E9E9E, scale: '0.5' } )
+    line = new THREE.Line( geometryLine, lineMaterial );
+    scene.add(line);
+
+  };
+
+  renderer2.render( scene2, camera);
+  renderer.render( scene, camera);
+  camera.updateProjectionMatrix();
+  controls.update();
+
+}; 
+
+
+function setPixelRatio(){
+  if (isMobileDevice() == true){
+    renderer.setPixelRatio( 1 );
+  } else {
+    renderer.setPixelRatio( 2.0 );
+  }
+};
+setPixelRatio();
+
+
 function onWindowResize() {
   var aspect = window.innerWidth / window.innerHeight;
   camera.left   = - frustumSize * aspect / 2;
@@ -180,7 +248,6 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight );
 };
-
 
 function isMobileDevice() {
     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
@@ -199,5 +266,3 @@ function uiReshuffle(){
     document.getElementById("title").style.borderBottom = "2px solid white";
   }
 };
-
-uiReshuffle();
