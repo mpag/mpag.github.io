44a2fa95792608aef32effa1b2dd762518eb51ef
mpag <mitchell.r.page@gmail.com>
2018-12-12 12:28:26 +1100

    Cleaned Up Pano Viewer with Cycling

--- mlc/mlcPano/js/baseFile.js
+++ mlc/mlcPano/js/baseFile.js
@@ -1,24 +1,51 @@
-var img = ["img/equirectangular.jpg", 'img/equirectangular2.jpg'];
+var img = ["img/equirectangular0.jpg", 'img/equirectangular3.jpg', 'img/equirectangular1.jpg', 'img/equirectangular2.jpg'];
+var imgDesc = ["Ground Floor of Attrium", "First Floor of Attrium", "First Floor Balcony", "Third Floor of Attrium"];
 var count = 0;
+
+
+//CALLS
+document.getElementById('previous').addEventListener('click', prevImage, false);
+document.getElementById('next').addEventListener('click', nextImage, false);
+document.getElementById('imageRef').innerHTML = "<h1>" + imgDesc[count] + "</h1>";
 animate();
 
+//ANIMATE
 function animate(){
   window.requestAnimationFrame( animate );
-  var time = new Date();
-  var timeInt = Math.floor(time.getSeconds()*0.1);
-
-  if (timeInt % 2 == 0){
-    count = 0;
-  } else {
-    count = 1
-  };
-
-  var rotVal = "0 " + Date.now()*0.001 + " 0"; 
-
-  document.querySelector('#sky').setAttribute('src', img[count]);
+  var rotVal = "0 " + Date.now()*0.0005 + " 0"; 
   document.querySelector('#sky').setAttribute('rotation', rotVal);
 };
+
+
+//MODAL
+document.querySelector('.close').addEventListener('click', function(){
+  document.querySelector('.bg-modal').style.display = 'none';
+});
+
+document.querySelector('.imageRef').addEventListener('click', function(){
+  document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+
+//IMAGE CYCLING
+function prevImage(){
+  count--;
+  document.querySelector('#sky').setAttribute('src', img[count%img.length]);
+  document.getElementById('imageRef').innerHTML = "<h1>" + imgDesc[count%img.length] + "</h1>";
+};
+
+function nextImage(){
+  count++;
+  document.querySelector('#sky').setAttribute('src', img[count%img.length]);
+  document.getElementById('imageRef').innerHTML = "<h1>" + imgDesc[count%img.length] + "</h1>";
+}
 
 function map_range(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 };
+
+
+//LOADING
+document.querySelector('a-scene').addEventListener('loaded', function () {
+  $("#loadingScreen").delay(1000).fadeOut(500);
+})
